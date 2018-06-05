09b00c7f039567e56e14b49e8df58f7aa7970b32
Leah Enelow <lenelow@students.terpmail.edu>
2018-06-05 14:10:45 -0400

    checkpoint second try

--- hof.js
+++ hof.js
@@ -20,10 +20,14 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleAges`.
 // Type your solution immediately below this line:
-
-
+var peopleAges = people.map(function(person){
+  return person.age
+})
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are WDI students. Assign the returned array
 // to a variable called `wdiStudents`.
 // Type your solution immediately below this line:
+var wdiStudents = people.filter(function(person){
+  return person.course.WDI
+})
--- oojs.js
+++ oojs.js
@@ -5,7 +5,15 @@
 // - a `members` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
+class Group {
+  constructor(name, members=[])
+    this.name = name
+    this.members = members
 
+    addMember(person) {
+    this.members.push('person')
+    }
+}
 
 
 
@@ -13,8 +21,8 @@
 // #2: Create an instance of the Group class and set it to a variable called `classmates`
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
-
-
+var classmates = new Group('name')
+classmates.addMember('person');
 
 
 
